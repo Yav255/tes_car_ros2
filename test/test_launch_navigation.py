2e9cf1ac6662a5188d842c2f4bfddafae7b8dff3
Vyass <87004439+Yav255@users.noreply.github.com>
2026-07-25 12:11:43 +0530

    Update test_launch_navigation.py

--- test/test_launch_navigation.py
+++ test/test_launch_navigation.py
@@ -42,7 +42,7 @@ class Test_talker_Integ(unittest.TestCase):
         self.node.create_subscription(String, "/chatter", msg_callback,10)
 
         start_time = time.time()
-        while time.time() - start_time < 5.0:
+        while time.time() - start_time < 15.0:
             rclpy.spin_once(self.node, timeout_sec=0.5)
             if len(received_messages) >0:
                 break
